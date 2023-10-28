be4b3f68a40c94b3fd4699231ee81048c7869e1c
hay <hayrdenhald@fastmail.com>
2023-10-28 21:43:02 +0200

    Add backend

--- backend/db.js
+++ backend/db.js
@@ -1,22 +1,24 @@
 import * as FM from "./fm.js";
 
-const dbPath = "./db.json";
+const dbPath = "./backend/db.json";
 
 const initialContent = {
   persons: [
     {
       name: "b",
-      deployAmount: 0,
+      deployAmount: 0
     },
     {
       name: "h",
-      deployAmount: 0,
+      deployAmount: 0
     },
     {
       name: "r",
-      deployAmount: 0,
-    },
-  ]
+      deployAmount: 0
+    }
+  ],
+  chosenPersonName: "",
+  lastUpdate: "27.10.23"
 };
 
 export async function initializeDb() {
@@ -36,6 +38,42 @@ export async function getPersons() {
 export async function setPersons(persons) {
   const db = await getDb();
   db.persons = persons;
+  const success = await setDb(db);
+  return success;
+}
+
+export async function getLastUpdate() {
+  const db = await getDb();
+  return db.lastUpdate;
+}
+
+export async function setLastUpdate(lastUpdate) {
+  const db = await getDb();
+  db.lastUpdate = lastUpdate;
+  const success = await setDb(db);
+  return success;
+}
+
+export async function getChosenPersonNameName() {
+  const db = await getDb();
+  return db.chosenPersonName;
+}
+
+export async function setChosenPersonName(chosenPersonName) {
+  const db = await getDb();
+  db.chosenPersonName = chosenPersonName;
+  const success = await setDb(db);
+  return success;
+}
+
+export async function getChoosingPool() {
+  const db = await getDb();
+  return db.choosingPool;
+}
+
+export async function setChoosingPool(choosingPool) {
+  const db = await getDb();
+  db.choosingPool = choosingPool;
   const success = await setDb(db);
   return success;
 }
--- backend/main.js
+++ backend/main.js
@@ -1,25 +1,65 @@
-(function main() {
-  console.log("Hello from main.js!");
+import * as DB from "./db.js";
+import * as FM from "./fm.js";
+import * as Utils from "./utils.js";
 
-  // const dbInitializationSuccess = await DB.initializeDb();
-  // if (!dbInitializationSuccess) {
-  //   throw new Error("Something went wrong with initialization of the db, exiting...");
-  // }
+(async function main() {
+  const dbInitializationSuccess = await DB.initializeDb();
+  if (!dbInitializationSuccess) {
+    throw new Error("Something went wrong with initialization of the db, exiting...");
+  }
 
-  // const persons = await DB.getPersons();
-  // console.log(persons);
+  const persons = await DB.getPersons();
+  const choosingPool = await DB.getChoosingPool();
 
+  if (!choosingPool.length) {
+    throw new Error("The choosing pool was empty!");
+  }
+
+  const chosenPersonName = choose(choosingPool);
+
+  let newChoosingPool = choosingPool.filter(x => x !== chosenPersonName);
+  if (!newChoosingPool?.length) {
+    newChoosingPool = persons.map(x => x.name);
+  }
+
+  await DB.setChoosingPool(newChoosingPool);
+
+  await DB.setChosenPersonName(chosenPersonName);
+
+  const lastUpdate = Utils.getCurrentSimpleDate();
+  await DB.setLastUpdate(lastUpdate);
+
+  const chosenPerson = persons.find(x => x.name === chosenPersonName);
+  const newPerson = incrementDeployAmount(chosenPerson);
+  const newPersons = persons.map(x => x.name === newPerson.name ? newPerson : x);
+
+  await DB.setPersons(newPersons);
+
+  updateFrontendContent(chosenPerson, lastUpdate, newPersons);
 })();
 
 function choose(choices) {
-  const index = Math.floor(Math.random() * choices.length);
-  return choices[index];
+  return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function incrementDeployAmount(person) {
   return { name: person.name, deployAmount: person.deployAmount + 1 }
 }
 
-function updatePersonInPersonData(person, personData) {
-  return personData.map(x => x.name === person.name ? person : x);
+async function updateFrontendContent(chosenPerson, lastUpdate, persons) {
+  let y = {};
+
+  persons.forEach(person => {
+    y[person.name] = person.deployAmount;
+  });
+
+  const content = {
+    chosenPersonName: chosenPerson.name,
+    lastUpdate,
+    deployAmounts: Object.fromEntries(
+      persons.map(person => [person.name, person.deployAmount])
+    ),
+  };
+
+  await FM.write("./frontend/content.json", JSON.stringify(content, null, "\t"));
 }
